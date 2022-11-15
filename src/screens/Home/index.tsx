1da68e66e4077bc81c946cc0e00f98270ffffa02
DayvsonAlmeida <dayvsonwilkson@gmail.com>
2022-11-15 18:13:45 -0300

    styles: use theme

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,23 +1,15 @@
-import { StyleSheet, ScrollView } from "react-native";
 import { DailyVerse } from "./DailyVerse";
 import { Shortcuts } from "./Shortcuts";
 import { Videos } from "./Videos";
 
+import { Container } from "./styles";
+
 export function Home() {
   return (
-    <ScrollView style={styles.Container}>
+    <Container>
       <Shortcuts />
       <DailyVerse />
       <Videos />
-    </ScrollView>
+    </Container>
   );
 }
-
-const styles = StyleSheet.create({
-  Container: {
-    flex: 1,
-    paddingTop: 24,
-    paddingHorizontal: 16,
-    width: "100%",
-  },
-});
---
+++ src/screens/Home/styles.ts
@@ -0,0 +1,8 @@
+import styled from "styled-components/native";
+
+export const Container = styled.ScrollView`
+  flex: 1;
+  width: 100%;
+  padding-top: 24px;
+  padding-horizontal: 16px;
+`;
