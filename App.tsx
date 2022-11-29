5999718f85a1bfbbb7708826610ed6d70326b3d6
DayvsonAlmeida <dayvsonwilkson@gmail.com>
2022-11-29 20:41:39 -0300

    feat: use loading component

--- App.tsx
+++ App.tsx
@@ -1,21 +1,22 @@
-import { ActivityIndicator } from "react-native";
-import { ThemeProvider } from "styled-components";
 import {
   useFonts,
   Roboto_400Regular,
   Roboto_700Bold,
 } from "@expo-google-fonts/roboto";
+import { ThemeProvider } from "styled-components";
 
-import theme from "./src/theme";
+import { Loading } from "@components/Loading";
 
 import { Home } from "@screens/Home";
+
+import theme from "./src/theme";
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
 
   return (
     <ThemeProvider theme={theme}>
-      {fontsLoaded ? <Home /> : <ActivityIndicator />}
+      {fontsLoaded ? <Home /> : <Loading />}
     </ThemeProvider>
   );
 }
