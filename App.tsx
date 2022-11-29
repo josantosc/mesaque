5fe175e1e1269e35b098f939821e5020a9dbbee4
DayvsonAlmeida <dayvsonwilkson@gmail.com>
2022-11-29 20:32:17 -0300

    feat: add roboto font

--- App.tsx
+++ App.tsx
@@ -1,13 +1,21 @@
+import { ActivityIndicator } from "react-native";
 import { ThemeProvider } from "styled-components";
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
 
 import theme from "./src/theme";
 
 import { Home } from "@screens/Home";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
   return (
     <ThemeProvider theme={theme}>
-      <Home />
+      {fontsLoaded ? <Home /> : <ActivityIndicator />}
     </ThemeProvider>
   );
 }
