c802f627c915c577032930abd985ec1dc353f3da
DayvsonAlmeida <dayvsonwilkson@gmail.com>
2022-11-30 00:14:03 -0300

    feat: add paragraph component

---
+++ src/components/Paragraph/styles.ts
@@ -0,0 +1,8 @@
+import styled from "styled-components/native";
+
+export const Content = styled.Text`
+  color: ${({ theme }) => theme.COLORS.GRAY_700};
+  font-size: ${({ theme }) => theme.FONT_SIZE.SM}px;
+  line-height: ${({ theme }) => theme.LINE_HEIGHT.SM}px;
+  font-family: ${({ theme }) => theme.FONT_FAMILY.REGULAR};
+`;
