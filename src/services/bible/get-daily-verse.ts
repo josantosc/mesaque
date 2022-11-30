e1c82421403fb26326dcd74f7f8187a0e4ad78f2
DayvsonAlmeida <dayvsonwilkson@gmail.com>
2022-11-30 20:40:24 -0300

    feat: add cache to get daily verse

--- src/services/bible/get-daily-verse.ts
+++ src/services/bible/get-daily-verse.ts
@@ -1,20 +1,26 @@
 import { Bible } from "@shared/domain/Bible";
+import { getVerse } from "@storage/daily-verse/get-verse";
+import { saveVerse } from "@storage/daily-verse/save-verse";
 
 export const getDailyVerse = async (): Promise<Bible.Verse> => {
+  const verseStoraged = await getVerse();
+
+  if (verseStoraged) return verseStoraged;
+
+  const verse: Bible.Verse = {
+    book: {
+      abbrev: "mt",
+      name: "Mateus",
+      testament: Bible.Testaments.New,
+    },
+    chapter: 8,
+    number: 2,
+    text: 'Um leproso, aproximando-se, adorou-o de joelhos e disse: "Senhor, se quiseres, podes purificar-me!"',
+  };
+
+  await saveVerse(verse);
+
   return new Promise((resolve, reject) => {
-    setTimeout(
-      () =>
-        resolve({
-          book: {
-            abbrev: "mt",
-            name: "Mateus",
-            testament: Bible.Testaments.New,
-          },
-          chapter: 8,
-          number: 2,
-          text: 'Um leproso, aproximando-se, adorou-o de joelhos e disse: "Senhor, se quiseres, podes purificar-me!"',
-        }),
-      5000
-    );
+    setTimeout(() => resolve(verse), 5000);
   });
 };
