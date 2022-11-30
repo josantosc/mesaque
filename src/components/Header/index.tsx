a0f7f69c48a29f92b07cced976bc5c810da06b1e
DayvsonAlmeida <dayvsonwilkson@gmail.com>
2022-11-29 22:04:16 -0300

    feat: add back button to header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,22 @@
+import Mdicons from "@expo/vector-icons/MaterialIcons";
+
 import logoImg from "@assets/favicon.png";
 
-import { Container, Logo } from "./styles";
+import { Container, Logo, BackButton, BackIcon } from "./styles";
 
-export function Header() {
+type Props = {
+  showBackButton?: boolean;
+};
+
+export function Header({ showBackButton = false }: Props) {
   return (
     <Container>
+      {showBackButton && (
+        <BackButton>
+          <BackIcon name="arrow-back-ios" />
+        </BackButton>
+      )}
+
       <Logo source={logoImg} />
     </Container>
   );
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,3 +1,4 @@
+import Mdicons from "@expo/vector-icons/MaterialIcons";
 import styled from "styled-components/native";
 
 export const Container = styled.View`
@@ -5,9 +6,20 @@ export const Container = styled.View`
   flex-direction: row;
   align-items: center;
   justify-content: center;
+
+  background-color: ${({ theme }) => theme.COLORS.GRAY_400};
 `;
 
 export const Logo = styled.Image`
   width: 46px;
   height: 55px;
+`;
+
+export const BackButton = styled.TouchableOpacity`
+  flex: 1;
+`;
+
+export const BackIcon = styled(Mdicons)`
+  color: ${({ theme }) => theme.COLORS.WHITE};
+  font-size: ${({ theme }) => theme.FONT_SIZE.XL};
 `;
