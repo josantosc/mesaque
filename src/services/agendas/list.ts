1a7d3ba88c6bb8bddcfee5bfaf2ad5bf43ca530e
DayvsonAlmeida <dayvsonwilkson@gmail.com>
2022-11-30 18:37:45 -0300

    fix: map new agenda field

--- src/services/agendas/list.ts
+++ src/services/agendas/list.ts
@@ -3,14 +3,14 @@ import { clientHttp } from "@services/client";
 
 type AgendaApi = {
   title: string;
-  data: number[];
+  date: number[];
   created_at: string;
 };
 
 type ApiResponse = Array<AgendaApi>;
 
 const toAgenda = (agenda: AgendaApi): Agenda => {
-  const days: Array<DaysOfWeek> = agenda.data;
+  const days: Array<DaysOfWeek> = agenda.date;
 
   return {
     days,
