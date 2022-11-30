7d5815c76117a223b4a25a35327c29e11fd492c6
DayvsonAlmeida <dayvsonwilkson@gmail.com>
2022-11-30 04:43:42 -0300

    refact: remove unused import

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -3,7 +3,6 @@ import { Shortcuts } from "./Shortcuts";
 import { Videos } from "./Videos";
 
 import { Container } from "./styles";
-import { Header } from "@components/Header";
 
 export function Home() {
   return (
